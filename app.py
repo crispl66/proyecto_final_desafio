e949326ef53a7fcd5c20b96768ed7823a4900f55
Blanca Lluch <lluchponceblanca@gmail.com>
2024-01-15 13:37:49 +0100

    archivo temporal para pdf

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, jsonify, render_template, request
-#from transformers import pipeline
 from gtts import gTTS
 import requests
 import os 
@@ -23,18 +22,41 @@ audios_collection = db['audios']
 
 @app.route('/', methods=['GET'])
 def plantilla():
-    return render_template ('endpoints.html')
+    return render_template('endpoints.html')
 
 @app.route('/subir_pdf', methods=['POST'])
 def subir_pdf():
+    if 'file' not in request.files:
+        return "No se proporcionó ningún archivo", 400
+
     file = request.files['file']
-    return file
 
-@app.route('/resumen', methods=['GET'])
-def resumen(file):
-    text = extract_text (file)
+    if not file:
+        return "El archivo está vacío", 400
+
+    # Ubicación temporal
+    file_path = os.path.join("temp", "uploaded_file.pdf")
+    file.save(file_path)
+
+    return "Archivo recibido correctamente", 200
+
+@app.route('/resumen', methods=['GET','POST'])
+def resumen():
+    #if 'file' not in request.files:
+    #    return "No se proporcionó ningún archivo"
+    #file = request.files['file']
+    #file_content = request.args.get('file_content')
+
+    file_path = os.path.join("temp", "uploaded_file.pdf")
+
+    if not os.path.exists(file_path):
+        return "No se encontró el archivo", 400
+    if not file:
+        return "No se proporcionó ningún archivo en el parámetro 'file'", 400
+
+    text = extract_text(file_path)
+    
     API_TOKEN = "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"
-
     API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
     headers = {"Authorization": f"Bearer {API_TOKEN}"}
 
@@ -42,10 +64,47 @@ def resumen(file):
         response = requests.post(API_URL, headers=headers, json=payload)
         return response.json()
 
-    resumen = query({"inputs":text})
+    resumen = query({"inputs": text})
     contenido_resumen = resumen[0][next(iter(resumen[0]))]
     resumen_collection.insert_one({'resumen': contenido_resumen})
-    return jsonify(contenido_resumen)
+    tts = gTTS(text=contenido_resumen, lang='es')
+    audio = tts.save("audio.mp3")
+    audios_collection.insert_one({'audio': audio})
+    
+    return jsonify({'resumen': contenido_resumen, 'audio': audio})
+
+'''@app.route('/subir_pdf', methods=['GET','POST'])
+def subir_pdf():
+    if 'file' not in request.files:
+        return "No se proporcionó ningún archivo"
+    file = request.files['file']
+    return file
+
+@app.route('/resumen', methods=['GET','POST'])
+def resumen():
+    #if 'file' not in request.files:
+    #    return "No se proporcionó ningún archivo"
+    #file = request.files['file']
+    file_content = request.args.get('file_content')
+
+    if not file_content:
+        return "No se proporcionó ningún archivo en el parámetro 'file_content'", 400
+
+    text = extract_text(file_content)
+    
+    API_TOKEN = "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"
+    API_URL = "https://api-inference.huggingface.co/models/facebook/bart-large-cnn"
+    headers = {"Authorization": f"Bearer {API_TOKEN}"}
+
+    def query(payload):
+        response = requests.post(API_URL, headers=headers, json=payload)
+        return response.json()
+
+    resumen = query({"inputs": text})
+    contenido_resumen = resumen[0][next(iter(resumen[0]))]
+    resumen_collection.insert_one({'resumen': contenido_resumen})
+    
+    return jsonify({'resumen': contenido_resumen})'''
 
 @app.route('/audio', methods=['GET'])
 def audio(contenido_resumen):
