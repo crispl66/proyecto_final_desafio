20b28b9afec3a98ed65b2b4885e635513cde3d9c
Blanca Lluch <lluchponceblanca@gmail.com>
2024-01-15 15:06:38 +0100

    nueva actualizacion

--- app.py
+++ app.py
@@ -16,23 +16,23 @@ try:
     upload_folder=os.path.join(
     path.replace("/file_folder",""),"tmp")
     os.makedirs(upload_folder, exist_ok=True)
-    app.config['upload_folder’] = upload_folder
+    app.config['upload_folder'] = upload_folder
 except Exception as e:
-    app.logger.info(“An error occurred while creating temp folder”)
-    app.logger.error(“Exception occurred : {}”.format(e))
-@app.route(‘/’)
+    app.logger.info('An error occurred while creating temp folder')
+    app.logger.error('Exception occurred : {}'.format(e))
+@app.route('/')
 def index():
-return Response(json.dumps({
-    “status”: True,
-    “code”: 200,
-    “message”: “Its Working!”}), mimetype=”application/json”)
-@app.route(‘/pass’, methods=[‘POST’])
+    return Response(json.dumps({
+    'status': True,
+    'code': 200,
+    'message': 'Its Working!'}), mimetype='application/json')
+@app.route('/pass', methods=['POST'])
 def post():
 try:
-    pdf_file = request.files[‘file’]
+    pdf_file = request.files['file']
     pdf_name = pdf_file.filename
     save_path = os.path.join(
-    app.config.get(‘upload_folder’),pdf_name)
+    app.config.get('upload_folder'),pdf_name)
     pdf_file.save(save_path)
     # getting file size
     file=Path(save_path).stat().st_size
